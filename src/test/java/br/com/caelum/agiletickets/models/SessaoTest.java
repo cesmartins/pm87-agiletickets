7d9c1b6a9044a5e0e717f701428eb37615f04641
mrobertomv <mrobertomv@yahoo.com.br>
2014-08-21 18:27:38 -0300

    novos testes classe sessao

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,14 +1,22 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.theories.suppliers.TestedOn;
 
 public class SessaoTest {
+	
+	Sessao sessao = null;
+	
+	
+	@Before
+	private void prepara(){
+		sessao = new Sessao();
+	}
 
 	@Test
 	public void podeVenderTodasVagasDisponiveis() throws Exception{
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(10);
 		
 		Assert.assertTrue(sessao.podeReservar(10));
@@ -16,7 +24,7 @@ public class SessaoTest {
 	
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
+		
         sessao.setTotalIngressos(2);
 
         Assert.assertTrue(sessao.podeReservar(1));
@@ -24,7 +32,6 @@ public class SessaoTest {
 	
 	@Test
 	public void deveVender5ingressosSeHa10vagas() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(10);
 		
 		Assert.assertTrue(sessao.podeReservar(5));
@@ -32,7 +39,6 @@ public class SessaoTest {
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
 
 		Assert.assertFalse(sessao.podeReservar(3));
@@ -40,11 +46,15 @@ public class SessaoTest {
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
+	}
+	
+	@Test
+	public void naoPodeReservarSessaoSemVagasDisponiveis() throws Exception{
+		 Sessao sessao = new Sessao();
 	}
 	
 }
